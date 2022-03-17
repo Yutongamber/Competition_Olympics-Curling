4ed25f2e151253b92cc5c690fb0d585c325cd222
Yutongamber <yutongamber@yahoo.com>
2022-03-17 20:25:14 +0800

    add rule

--- agents/rule/submission.py
+++ agents/rule/submission.py
@@ -1,21 +1,132 @@
 # -*- coding:utf-8  -*-
-# Time  : 2022/1/29 上午10:48
-# Author: Yahui Cui
-import argparse
-import os
-import sys
+# Time  : 2022/3/27 下午: 16:00
+# Author: Yutongamber
 
 
-# actions_map = {0: [-100, -30], 1: [-100, -18], 2: [-100, -6], 3: [-100, 6], 4: [-100, 18], 5: [-100, 30], 6: [-40, -30],
-#                7: [-40, -18], 8: [-40, -6], 9: [-40, 6], 10: [-40, 18], 11: [-40, 30], 12: [20, -30], 13: [20, -18],
-#                14: [20, -6], 15: [20, 6], 16: [20, 18], 17: [20, 30], 18: [80, -30], 19: [80, -18], 20: [80, -6],
-#                21: [80, 6], 22: [80, 18], 23: [80, 30], 24: [140, -30], 25: [140, -18], 26: [140, -6], 27: [140, 6],
-#                28: [140, 18], 29: [140, 30], 30: [200, -30], 31: [200, -18], 32: [200, -6], 33: [200, 6], 34: [200, 18],
-#                35: [200, 30]}
+import numpy as np
+import math
+import random
+
+IDX_TO_COLOR = {
+    0: 'light green',
+    1: 'green',
+    2: 'sky blue',
+    3: 'yellow',
+    4: 'grey',
+    5: 'purple',
+    6: 'black',
+    7: 'red',
+    8: 'blue'
+}
+
+
+class rule_agent:
+    def __init__(self):
+        self.count = 0
+        self.obs = None
+        self.obs_key = None
+        self.ball_left = None
+        self.ball_first = 0
+        self.mark = 30 # hyper
+        self.contrl_player = None
+        self.ball_oppo_pos = []
+        self.action_mark = None
+        self.actions_maybe_good = [[0.1, 30], [5, 35], [-5, 35], [0.1, 40]]
+
+    def rolling_reset(self):
+        self.count = 0
+        self.action_mark = None
+
+    def round_reset(self):
+        self.count = 0
+        self.ball_first = 0
+        self.action_mark = None
+        self.actions_maybe_good = [[0.1, 30], [5, 35], [-5, 35], [0.1, 40]]
+
+    def get_obs(self, observation):
+        ball_left_current = observation['throws left'][observation['controlled_player_index']]
+
+        if self.ball_first == 0:
+            self.contrl_player = observation['controlled_player_index']
+
+        if  self.count != 0 and ball_left_current != self.ball_left:
+            self.rolling_reset()
+            self.ball_first += 1
+
+        if self.ball_first == 1 and self.count == self.mark:
+            self.obs_key = observation
+
+        if ball_left_current == 4:
+            self.round_reset()
+
+        self.obs = observation
+        self.ball_left = self.obs['throws left'][observation['controlled_player_index']]
+
+    def analyze(self):
+        angle = 0
+        force = 200
+        if self.count == self.mark:
+            idxs = np.where(self.obs['obs'][0] == 1)
+            if len(idxs[0]) > 0:
+                xs, ys = idxs[0], idxs[1]
+                oppo_ball_already = 4 - self.obs['throws left'][1] # TODO
+                avg_x_poss, avg_y_poss = None, None
+                if oppo_ball_already > 1:
+                    last_idx = 0
+                    dist = 100
+                    for i in range(len(xs)):
+                        if i == (len(xs)-1) or abs(xs[i] - xs[i+1]) > 2 or abs(ys[i] - ys[i+1]):
+                            avg_x, avg_y = np.mean(xs[last_idx:i+1]), np.mean(ys[last_idx:i+1])
+                            last_idx = i+1
+                            if (avg_x - 8.5) ** 2 + (avg_y - 14.5) ** 2 < dist:
+                                dist = (avg_x - 8.5) ** 2 + (avg_y - 14.5) ** 2
+                                avg_x_poss, avg_y_poss = avg_x, avg_y
+                else:
+                    avg_x, avg_y = np.mean(xs), np.mean(ys)
+                    if (avg_x - 8.5) ** 2 + (avg_y - 14.5) ** 2 < 100:
+                        avg_x_poss, avg_y_poss = avg_x, avg_y
+                if not avg_x_poss:
+                    angle = 0
+                else:
+                    angle = -180*math.atan((15 - avg_y_poss)/ (30 - avg_x_poss))/math.pi
+                if abs(angle) > 30:
+                    angle = 30 if angle > 0 else -30
+
+            if angle == 0:
+                action = self.actions_maybe_good.pop()
+                angle, force = action[0], action[1]
+            self.action_mark = (angle, force)
+
+        elif self.count > self.mark:
+            if self.action_mark[1] == 200:
+                angle, force = 0, 200
+            else:
+                angle, force = 0, self.action_mark[1]
+        return angle, force
+
+    def step(self):
+        angle, force = self.analyze()
+        if self.count <= 3:
+            action = [160, 0]
+        elif 3 < self.count <= 4:
+            action = [-100, 0]
+        elif self.count > self.mark:
+            action = [force, 0]
+        elif self.count == self.mark:
+            action = [0, angle]
+        else:
+            action = [0, 0]
+        self.count += 1
+        return action
+
+
+rule = rule_agent()
+
 
 def my_controller(observation, action_space, is_act_continuous=False):
     # inference
-    action_ctrl = [51.1, 0]
+    rule.get_obs(observation)
+    action_ctrl = rule.step()
     agent_action = [[action_ctrl[0]], [action_ctrl[1]]]  # wrapping up the action
 
     return agent_action
--- run_log.py
+++ run_log.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--my_ai", default="random", help="rl/random")
-    parser.add_argument("--opponent", default="rl", help="rl/random")
+    parser.add_argument("--opponent", default="rule", help="rl/random")
     args = parser.parse_args()
 
     # policy_list = ["random"] * len(game.agent_nums)
